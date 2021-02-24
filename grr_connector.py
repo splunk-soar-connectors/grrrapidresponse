9c9d2ba9cc11f53b4cd6b1976c6c3413e0c31345
bvaghela <bvaghela@splunk.com>
2021-02-24 15:56:09 +0530

    PAPP-13823-Py2to3

--- grr_connector.py
+++ grr_connector.py
@@ -1,16 +1,8 @@
-# --
 # File: grr_connector.py
+# Copyright (c) 2018-2021 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2018
-#
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 
 # Phantom App imports
 import phantom.app as phantom
@@ -203,8 +195,16 @@ class GrrConnector(BaseConnector):
 
         self.save_progress("Verifying response")
 
-        if not r:
-            return action_result.set_status(phantom.APP_ERROR, r.text), None
+        if not r.ok:
+            message = "Can't process response from server. Status Code: {0} Message: {1}"
+            try:
+                resp_json = r.text.lstrip(")]}'\n")
+                resp_json = json.loads(resp_json)
+                message = message.format(r.status_code, resp_json.get('message', r.reason))
+            except Exception as e:
+                return action_result.set_status(phantom.APP_ERROR, GRR_ERR_JSON_PARSE, e), None
+
+            return action_result.set_status(phantom.APP_ERROR, message), None
 
         try:
             resp_json = json.loads(r.text.splitlines()[1])
@@ -437,7 +437,7 @@ class GrrConnector(BaseConnector):
 
         # Add a dictionary that is made up of the most important values from data into the summary
         summary = action_result.update_summary({})
-        summary['total_hunts'] = len(response)
+        summary['total_hunts'] = len(response.get('items', {}))
 
         # Return success, no need to set the message, only the status
         # BaseConnector will create a textual message based off of the summary dictionary
@@ -504,7 +504,7 @@ class GrrConnector(BaseConnector):
         body = {
             "flow": {
                 "args": {
-                    "paths": [ file_path ],
+                    "paths": [file_path],
                     "@type": "type.googleapis.com/FileFinderArgs"
                 },
                 "name": "FileFinder",
@@ -562,11 +562,11 @@ class GrrConnector(BaseConnector):
         # Required values can be accessed directly
         client_id = param[GRR_JSON_CLIENT_ID]
         regex = param[GRR_JSON_BROWSER_CACHE_REGEX]
-        users = [x.strip() for x in str(param[GRR_JSON_USERS]).split(",")]  # might need to format if multiple users
+        users = [x.strip() for x in str(param[GRR_JSON_USERS]).split(",") if x.strip()]  # might need to format if multiple users
 
-        check_chrome = param.get('check_chrome', True)
-        check_firefox = param.get('check_firefox', True)
-        check_ie = param.get('check_ie', True)
+        check_chrome = True if str(param.get('check_chrome', True)).lower() == 'true' else False
+        check_firefox = True if str(param.get('check_firefox', True)).lower() == 'true' else False
+        check_ie = True if str(param.get('check_ie', True)).lower() == 'true' else False
 
         endpoint = "/api/v2/clients/{0}/flows".format(client_id)
 
@@ -686,8 +686,10 @@ if __name__ == '__main__':
 
     if (username and password):
         try:
-            print ("Accessing the Login page")
-            r = requests.get("https://127.0.0.1/login", verify=False)
+            login_url = AwsEc2Connector._get_phantom_base_url() + '/login'
+
+            print("Accessing the Login page")
+            r = requests.get(login_url, verify=False)
             csrftoken = r.cookies['csrftoken']
 
             data = dict()
@@ -697,13 +699,13 @@ if __name__ == '__main__':
 
             headers = dict()
             headers['Cookie'] = 'csrftoken=' + csrftoken
-            headers['Referer'] = 'https://127.0.0.1/login'
+            headers['Referer'] = login_url
 
-            print ("Logging into Platform to get the session id")
-            r2 = requests.post("https://127.0.0.1/login", verify=False, data=data, headers=headers)
+            print("Logging into Platform to get the session id")
+            r2 = requests.post(login_url, verify=False, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
-            print ("Unable to get session id from the platfrom. Error: " + str(e))
+            print("Unable to get session id from the platfrom. Error: " + str(e))
             exit(1)
 
     with open(args.input_test_json) as f:
@@ -719,6 +721,6 @@ if __name__ == '__main__':
             connector._set_csrf_info(csrftoken, headers['Referer'])
 
         ret_val = connector._handle_action(json.dumps(in_json), None)
-        print (json.dumps(json.loads(ret_val), indent=4))
+        print(json.dumps(json.loads(ret_val), indent=4))
 
     exit(0)
